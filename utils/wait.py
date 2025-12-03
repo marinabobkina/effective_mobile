e1aacfc032109a209cfbbfb4d74513f715a9ff0f
Марина Бобкина <bmbod@mail.ru>
2025-12-03 17:11:00 +0300

    Add wait.py

---
+++ utils/wait.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def wait_for(driver, condition, timeout):
+    return WebDriverWait(driver, timeout).until(condition)
+
+
+def clickable(locator):
+    return EC.element_to_be_clickable(locator)
+
+
+def visible(locator):
+    return EC.visibility_of_element_located(locator)
+
+
+def url_contains(part):
+    return EC.url_contains(part)
