a4fef4336d347dc45ce12224f3ae5feb2329edfc
Марина Бобкина <bmbod@mail.ru>
2025-12-04 16:29:16 +0300

    Add --headless, --no-sandbox, --disable-dev-shm-usage

--- conftest.py
+++ conftest.py
@@ -9,6 +9,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 def driver(request):
     options = Options()
     options.add_argument(f"--window-size=1920,1080")
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(
         service=ChromeService(ChromeDriverManager().install()), options=options
     )
